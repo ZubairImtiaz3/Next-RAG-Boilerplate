37dece60c6111d72850aa7169cdbbf83c6b23a19
Zubair Imtiaz <106450046+ZubairImtiaz3@users.noreply.github.com>
2024-11-09 11:07:43 +0500

    Update route to use groq

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,14 +1,20 @@
-import { openai } from "@ai-sdk/openai";
+import { createOpenAI as createGroq } from "@ai-sdk/openai";
 import { streamText, convertToCoreMessages } from "ai";
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
 
+const groqAI = createGroq({
+  apiKey: process.env.GROQ_API_KEY,
+  baseURL: "https://api.groq.com/openai/v1",
+  compatibility: "compatible",
+});
+
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
   const result = await streamText({
-    model: openai("gpt-4-turbo"),
+    model: groqAI("llama3-70b-8192"),
     messages: convertToCoreMessages(messages),
   });
 
